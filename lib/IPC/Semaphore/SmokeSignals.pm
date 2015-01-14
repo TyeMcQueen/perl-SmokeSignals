37b960dee5d65ca4f180b35dece94c4df4fa49ec
Tye McQueen <tyemq@cpan.org>
2015-01-13 23:13:33 -0800

    Expand the POD documentation.

--- lib/IPC/Semaphore/SmokeSignals.pm
+++ lib/IPC/Semaphore/SmokeSignals.pm
@@ -223,8 +223,8 @@ It is easiest to use as a very simple mutex (see Synopsis above).
 
 You can also use this as a semaphore on a relatively small number of relatively
 small tokins (each tokin' must be the same number of bytes and the total
-number of bytes should be less than your system buffer size or else things
-will hang).
+number of bytes should be less than your pipe's capacity or else you're in
+for a bad trip).
 
 It also happens to give out tokins in LRU order (least recently used).
 
@@ -255,11 +255,139 @@ To use it as a semaphore / LRU:
         ...
     }
 
+=head1 EXPORTS
+
+There is 1 function that you can request to be exported into your package.
+It serves to prevent you from having to type the rather long module name
+(IPC::Semaphore::SmokeSignals) more than once.
+
+=head2 LightUp
+
+C<LightUp()> activates a new pipe for coordinating that only $N things can
+happen at once.
+
+    use IPC::Semaphore::SmokeSignals 'LightUp';
+
+    $pipe = LightUp( $fuel );
+
+There are several ways you can specify C<$fuel>:
+
+    my $pipe = LightUp();
+    # same as:
+    my $pipe = LightUp(1);
+
+    my $pipe = LightUp(50);
+    # same as:
+    my $pipe = LightUp(['01'..'50']);
+
+C<LightUp(...)> is just short for:
+
+    IPC::Semaphore::SmokeSignals->Ignite(...);
+
+An un-named pipe is used.  This has the advantages of requiring no clean-up
+and having no chance of colliding identifiers (unlike with SysV semaphores).
+
+The only argument, C<$fuel>, if given, should be one of:
+
+=over
+
+=item A false value
+
+This is the same as passing in a '1'.
+
+=item An array reference
+
+The array should contain 1 or more strings, all having the same length (in
+bytes).
+
+=item A positive integer
+
+Passing in C<$N> gives you C<$N> tokins each of length C<length($N)>.  So
+C<12> is the same as C<['01'..'12']>.
+
+=back
+
+=head1 METHODS
+
+=head2 Ignite
+
+    my $pipe = IPC::Semaphore::SmokeSignals->Ignite( $fuel );
+
+See L<LightUp>.
+
+=head2 Puff
+
+    my $dragon = $pipe->Puff();
+
+    my $dragon = $pipe->Puff('impatient');
+
+C<Puff()> takes a drag on your pipe and stores the tokin' it gets in a magic
+dragon that it gives to you.  Store the dragon in a lexical variable so that
+when you leave the scope of that variable, the tokin' will automatically be
+returned to the pipe (when the variable holding the dragon is destroyed),
+making that tokin' available to some other pipe user.
+
+The usual case is to use a semaphore to protect a block of code from being
+run by too many processes (or threads) at the same time.
+
+If you need to keep your tokin' reserved beyond any lexical scope containing
+your call to C<Puff()>, then you can pass the dragon around, even making
+copies of it.  When the last copy is destroyed, the tokin' will be returned.
+Or you can release it early by calling C<Exhale()> on it.
+
+If there are no available tokins, then the call to C<Puff()> will block,
+waiting for a tokin' to become available.  Alternately, you can pass in a
+true value as the only argument to C<Puff()> and this will cause C<Puff()>
+to return immediately, either returning a magic dragon containing a tokin'
+or just returning a false value.
+
+For example:
+
+    {
+        my $dragon = $pipe->Puff('impatient');
+        if( ! $dragon ) {
+            warn "Can't do that right now.\n";
+        } else {
+            # This code must never run more than $N times at once:
+            ...
+        }
+    }
+
+=head2 Sniff
+
+    my $tokin = $dragon->Sniff();
+
+Calling C<Sniff()> on a magic dragon returned from C<Puff()> will let you see
+the value of the tokin' that you have reserved.
+
+Calling C<Sniff()> on a magic dragon that has already had C<Exhale()> called
+on it will return C<undef>.
+
+=head2 Exhale
+
+    $dragon->Exhale();
+
+Calling C<Exhale()> on a magic dragon returned from C<Puff()> causes the
+dragon to release the reserved tokin' immediately.
+
+This can also be done by just overwriting the dragon, for example:
+
+    $dragon = undef;
+
+but only if C<$dragon> is the last/only existing copy of the dragon.
+
+=head2 Extinguish
+
+    $pipe->Extinguish();
+
+    my $leftovers = $pipe->Extinguish( 'impatient' );
+
+C<Extinguish()> marks the pipe as being shut down and starts pulling out and
+discarding all of the tokins in it.
 
 =head1 PLANS
 
-A future version will allow for non-blocking checking as to whether there are
-any tokins currently available and for setting a maximum wait time.
+A future version may allow for setting a maximum wait time.
 
 A future version will allow for using a named pipe to make it easy for several
 processes to share one pipe.
